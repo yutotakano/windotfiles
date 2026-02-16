ebf1aef71f7c37d37a71d230991b582b03e9f520
Yuto Takano <moa17stock@gmail.com>
2026-02-16 13:59:24 +0100

    Add a quick script to install various winget necessities

---
+++ winget_installs.bat
@@ -0,0 +1,53 @@
+winget install Schniz.fnm
+winget install GitHub.cli
+winget install fd
+winget install zyedidia.micro
+winget install typst
+winget install Starship
+winget install Kopia.KopiaUI
+winget install Rustlang.Rustup
+winget install uutils.coreutils
+winget install -e --id Ninja-build.Ninja
+winget install -e --id NASM.NASM
+winget install Kitware.Cmake
+winget install -e --id GnuWin32.Make
+winget install -e --id=GoLang.Go
+winget install mkvtoolnix
+winget install -e --id ImageMagick.ImageMagick
+winget install XAMPPRocky.tokei
+
+winget install -e --id WiresharkFoundation.Wireshark
+
+winget install ajeetdsouza.zoxide
+winget install Microsoft.OpenJDK.21
+winget install Microsoft.OpenJDK.17
+winget install -e --id chrisant996.Clink
+winget install 7zip.7zip
+winget install -h -e --id "WinFsp.WinFsp"
+winget install -h -e --id "SSHFS-Win.SSHFS-Win"
+
+winget install PrismLauncher.PrismLauncher
+winget install Anki.Anki
+winget install Doist.Todoist
+winget install Voidstar.FilePilot
+winget install BrickLink.Studio
+winget install BrickLink.PartDesigner
+winget install OBSProject.OBSStudio
+winget install f3d-app.f3d
+winget install SumatraPDF.SumatraPDF
+winget install IrfanSkiljan.IrfanView
+winget install darktable.darktable
+winget install High-Logic.MainType
+winget install Wacom.WacomTabletDriver
+winget install Resplendence.WhoCrashed
+winget install Microsoft.PowerToys
+winget install Highresolution.X-MouseButtonControl
+winget install Zoom.Zoom.EXE
+winget install JackieLiu.NotepadsApp
+winget install Microsoft.WindowsTerminal
+winget install Spotify.Spotify
+
+rem May need to setup clink's clink_start.cmd to load fnm env
+fnm install v24.13.1
+fnm use v24.13.1
+npm install -g yarn

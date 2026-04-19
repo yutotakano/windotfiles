8adaaa18e571d1c8108f37edb627d5d70ddd5c2b
Yuto Takano <moa17stock@gmail.com>
2026-04-19 04:53:20 -0700

    Reorder packages into essentials and non-essentials

--- winget_installs.bat
+++ winget_installs.bat
@@ -1,41 +1,43 @@
-rem Standard runtimes
+rem Essentials
 winget install -e --id Microsoft.VCRedist.2015+.x64
-
-rem Dev env setup
-winget install Git.Git
 winget install jdx.mise
+winget install Git.Git
 winget install GitHub.cli
 winget install fd
 winget install zyedidia.micro
-winget install typst
 winget install Starship
-winget install Kopia.KopiaUI
-winget install Rustlang.Rustup
 winget install uutils.coreutils
+winget install ajeetdsouza.zoxide
+winget install -e --id chrisant996.Clink
+
+rem Apps
+winget install Martchus.syncthingtray
+winget install WiresharkFoundation.Wireshark
+winget install 7zip.7zip
+winget install SumatraPDF.SumatraPDF
+winget install IrfanSkiljan.IrfanView
+winget install Kopia.KopiaUI
+
+rem Only when required (dev)
 winget install -e --id Ninja-build.Ninja
 winget install -e --id NASM.NASM
 winget install Kitware.Cmake
 winget install -e --id GnuWin32.Make
 winget install -e --id=GoLang.Go
+winget install Rustlang.Rustup
+winget install Microsoft.OpenJDK.21
+winget install Microsoft.OpenJDK.17
+
+rem Only when required (utils)
 winget install mkvtoolnix
 winget install -e --id ImageMagick.ImageMagick
 winget install XAMPPRocky.tokei
-
-winget install -e --id Martchus.syncthingtray
-
-rem Extras
-winget install -e --id WiresharkFoundation.Wireshark
-
-rem More extras
-winget install ajeetdsouza.zoxide
-winget install Microsoft.OpenJDK.21
-winget install Microsoft.OpenJDK.17
-winget install -e --id chrisant996.Clink
-winget install 7zip.7zip
+winget install typst
 winget install -h -e --id "WinFsp.WinFsp"
 winget install -h -e --id "SSHFS-Win.SSHFS-Win"
 
-rem Essentials
+rem Only when required (apps)
+winget install Spotify.Spotify
 winget install PrismLauncher.PrismLauncher
 winget install Anki.Anki
 winget install Doist.Todoist
@@ -43,8 +45,6 @@ winget install Voidstar.FilePilot
 winget install BrickLink.Studio
 winget install BrickLink.PartDesigner
 winget install OBSProject.OBSStudio
-winget install SumatraPDF.SumatraPDF
-winget install IrfanSkiljan.IrfanView
 winget install darktable.darktable
 winget install High-Logic.MainType
 winget install Wacom.WacomTabletDriver
@@ -53,9 +53,4 @@ winget install Microsoft.PowerToys
 winget install Highresolution.X-MouseButtonControl
 winget install Zoom.Zoom.EXE
 winget install JackieLiu.NotepadsApp
-winget install Microsoft.WindowsTerminal
-winget install Spotify.Spotify
 
-rem May need to setup clink's clink_start.cmd to load fnm env
-mise install -g node@24
-mise use -g node@20
